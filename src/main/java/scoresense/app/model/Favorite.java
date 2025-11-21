baf898209d8a37a0aa2710258f4cf725e42f9579
Maury <mauricioguerrero7@hotmail.com>
2025-11-21 15:59:02 -0600

    Favorite Complete

---
+++ src/main/java/scoresense/app/model/Favorite.java
@@ -0,0 +1,39 @@
+package scoresense.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "favorite_id")
+    private Long favoriteId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // Relación con la tabla users
+
+    @Column(name = "entity_type", nullable = false, length = 20)
+    private String entityType; // 'team' o 'player'
+
+    @Column(name = "entity_id", nullable = false)
+    private Long entityId; // ID del equipo o jugador favorito
+}
